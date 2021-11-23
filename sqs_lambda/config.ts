153ab0762bfa735fa5bb9df381a4c3f7fdca4a5d
Kat Schelonka <kschelonka@mozilla.com>
2021-11-23 15:48:01 -0800

    feat(user delete): make appropriate client-api request from events queue
    INFRA-159

--- sqs_lambda/config.ts
+++ sqs_lambda/config.ts
@@ -1,11 +1,17 @@
+const environment = process.env.ENVIRONMENT || 'development';
+const isDev = environment === 'development';
+
 const config = {
-  environment: process.env.ENVIRONMENT || 'development',
+  environment: environment,
   sentry: {
     // these values are inserted into the environment in
     // .aws/src/sqsLambda.ts
     dsn: process.env.SENTRY_DSN || '',
     release: process.env.GIT_SHA || '',
   },
+  clientApiUri: isDev
+    ? process.env.CLIENT_API_URI || 'https://client-api.getpocket.dev'
+    : process.env.CLIENT_API_URI || 'https://client-api.readitlater.com',
 };
 
 export default config;
