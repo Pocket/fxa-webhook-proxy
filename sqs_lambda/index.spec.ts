64cff5c379800c777d96cc92f9bc75f840da237e
Kat Schelonka <34227334+kschelonka@users.noreply.github.com>
2021-11-23 16:33:04 -0800

    feat(user delete): make appropriate client-api request from events queue (#41)
    INFRA-159

---
+++ sqs_lambda/index.spec.ts
@@ -0,0 +1,41 @@
+import * as fx from './index';
+import config from './config';
+import nock from 'nock';
+
+describe('SQS Event Handler', () => {
+  afterAll(() => {
+    nock.restore();
+  });
+  it('sends a user delete event to client-api', async () => {
+    const scope = nock(config.clientApiUri)
+      .post('/')
+      .reply(200, { data: { deleteUserByFxaId: '12345' } });
+    await fx.handlerFn({
+      Records: [
+        { user_id: '12345', event: fx.EVENT.USER_DELETE, timestamp: 12345 },
+      ],
+    });
+    // Nock marks as done if a request was successfully intercepted
+    expect(scope.isDone()).toBeTruthy();
+  });
+  it('throws an error if error data is returned from client-api', async () => {
+    const replyData = { data: null, errors: { CODE: 'FORBIDDEN' } };
+    const scope = nock(config.clientApiUri).post('/').reply(200, replyData);
+    const record = {
+      user_id: '12345',
+      event: fx.EVENT.USER_DELETE,
+      timestamp: 12345,
+    };
+    await expect(async () => {
+      await fx.handlerFn({
+        Records: [record],
+      });
+    }).rejects.toThrow(
+      `Error processing ${JSON.stringify(record)}: \n${JSON.stringify(
+        replyData.errors
+      )}`
+    );
+    // Nock marks as done if a request was successfully intercepted
+    expect(scope.isDone()).toBeTruthy();
+  });
+});
--- sqs_lambda/index.ts
+++ sqs_lambda/index.ts
@@ -1,12 +1,68 @@
 import * as Sentry from '@sentry/serverless';
 import config from './config';
+import fetch from 'node-fetch';
+
+// Not DRY -- try lambda layers?
+export enum EVENT {
+  USER_DELETE = 'user_delete',
+  PROFILE_UPDATE = 'profile_update',
+}
+
+type SqsEvent = {
+  user_id: string;
+  event: EVENT;
+  timestamp: number;
+};
+
+/**
+ * Submit deleteUserByFxaId mutation POST request to client-api
+ * @param id FxA account ID to delete from Pocket's database
+ */
+async function submitDeleteMutation(id: string): Promise<any> {
+  const deleteMutation = `
+mutation deleteUser($id: ID!) {
+  deleteUserByFxaId(id: $id)
+}`;
+  const variables = { id: id };
+  return await fetch(config.clientApiUri, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      // TODO: INFRA-169
+      // Authorization: jwt,
+    },
+    body: JSON.stringify({ query: deleteMutation, variables }),
+  }).then((response) => response.json());
+}
+
+/**
+ * Lambda handler function. Separated from the Sentry wrapper
+ * to make unit-testing easier.
+ * Takes records from SQS queue with events, and makes
+ * the appropriate request against client-api.
+ * TODO: Authorization bearer - INFRA-169
+ */
+export async function handlerFn(event: { Records: SqsEvent[] }) {
+  await Promise.all(
+    event.Records.map(async (record: SqsEvent) => {
+      if (record.event === EVENT.USER_DELETE) {
+        const res = await submitDeleteMutation(record.user_id);
+        if (res?.errors) {
+          throw new Error(
+            `Error processing ${JSON.stringify(record)}: \n${JSON.stringify(
+              res?.errors
+            )}`
+          );
+        }
+      }
+    })
+  );
+  return {};
+}
 
 Sentry.AWSLambda.init({
   dsn: config.sentry.dsn,
   release: config.sentry.release,
   environment: config.environment,
 });
-
-export const handler = Sentry.AWSLambda.wrapHandler(async (event: any) => {
-  console.log('I will handle it');
-});
+export const handler = Sentry.AWSLambda.wrapHandler(handlerFn);
