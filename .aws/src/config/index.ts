68293538a0e5fb5d9f84bf39e4ca33376e097325
Kelvin Yeboah <kyeboah@getpocket.com>
2021-12-06 13:30:16 -0800

    feat(API Gateway): Create domain for API Gateway (#63)

--- .aws/src/config/index.ts
+++ .aws/src/config/index.ts
@@ -1,10 +1,13 @@
 const name = 'FxAWebhookProxy';
 const isDev = process.env.NODE_ENV === 'development';
 const environment = isDev ? 'Dev' : 'Prod';
+const domainPrefix = 'fxa-events';
+const domain = `${domainPrefix}.${isDev ? 'getpocket.dev' : 'getpocket.com'}`;
 
 export const config = {
   name,
   isDev,
+  domain,
   prefix: `${name}-${environment}`,
   circleCIPrefix: `/${name}/CircleCI/${environment}`,
   shortName: 'FXAWP',
