c564cd1c02fb3833f09e7c62a68735814e1e7afa
Daniel Brooks <bass_rock@me.com>
2023-07-11 20:49:24 -0700

    fix(throttle): removing throttling

--- .aws/src/sqsLambda.ts
+++ .aws/src/sqsLambda.ts
@@ -34,7 +34,6 @@ export class SqsLambda extends Construct {
         runtime: LAMBDA_RUNTIMES.NODEJS14,
         handler: 'index.handler',
         timeout: 120,
-        reservedConcurrencyLimit: 10, // only allow 10 executations at a time in case FxA suddenly sends us a lot of SQS messages
         environment: {
           REGION: vpc.region,
           JWT_KEY: config.sqsLambda.jwtKey,
